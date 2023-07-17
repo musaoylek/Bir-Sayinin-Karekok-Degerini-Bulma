1e05a1d5cf8dd77ec4062e2655e75191dff8f504
musaoylek <101888206+musaoylek@users.noreply.github.com>
2023-07-17 10:44:41 +0300

    sqrt().js

---
+++ sqrt().js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>musaoylek sqrt() metodu kullanımı</title>
+</head>
+<body>
+	<script>
+
+		// sqrt(); kendisine verilen sayı değerinin karekök değerini bulur.
+		// Math.sqrt(); şeklinde kullanılır
+		
+		var sayi;
+		sayi= prompt("Lütfen bir sayı giriniz")
+		var karekok = Math.sqrt(sayi);
+		alert("Sayının karekök değeri : " +karekok);
+	</script>
+</body>
+</html>
